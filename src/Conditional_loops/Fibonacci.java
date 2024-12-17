c87b48ba5c71ced3fa769cc7ae625aa63df63799
Goutham Mallya G <gouthammallya2@gmail.com>
2024-12-17 19:21:42 +0530

    Completed the 21st assignmnet

---
+++ src/Conditional_loops/Fibonacci.java
@@ -0,0 +1,25 @@
+package Conditional_loops;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int a = 0;
+        int b = 1;
+        System.out.print("Enter the index of fibonacci : ");
+        int n = in.nextInt();
+        int index = n;
+        if(n<1){
+            System.out.println(0);
+        }else {
+            while (n - 2 > 0) {
+                int temp = b;
+                b = a + b;
+                a = temp;
+                n--;
+            }
+            System.out.println(index + "th fibonacci number is : " + b);
+        }
+    }
+}
